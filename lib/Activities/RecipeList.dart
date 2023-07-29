005d8461befea39a25d6d80d06c3d28c3a0dda36
Muhammad Ali <mdalinoorka@gmail.com>
2023-07-29 09:08:33 +0500

    Basic Complete

--- lib/Activities/RecipeList.dart
+++ lib/Activities/RecipeList.dart
@@ -28,84 +28,78 @@ class _RecipeListState extends State<RecipeList> {
         
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: heightPercent(context, 3.5)),
-          child: Column(
-            children: [
-              ListView.builder(
-                  itemCount: widget.recipes.length,
-                  shrinkWrap: true,
-                  // physics: NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    // Align: Alignment.topCenter;
-                    return InkWell(
-                        onTap: () {
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Recipe(widget.recipes[index].appUrl)));
-                        },
-                        child: Card(
-                            margin: EdgeInsets.symmetric(
-                                vertical: heightPercent(context, 2.5)),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
+          child:  ListView.builder(
+              itemCount: widget.recipes.length,
+              shrinkWrap: true,
+              // physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                // Align: Alignment.topCenter;
+                return InkWell(
+                    onTap: () {
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Recipe(widget.recipes[index].appUrl)));
+                    },
+                    child: Card(
+                        margin: EdgeInsets.symmetric(
+                            vertical: heightPercent(context, 2.5)),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        elevation: 0,
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              child: Image.network(
+                                widget.recipes[index].imageUrl,
+                                fit: BoxFit.cover,
+                                width: widthPercent(context, 100),
+                                height: heightPercent(context, 25),
+                              ),
+                              borderRadius:
+                              BorderRadius.circular(20),
                             ),
-                            elevation: 0,
-                            child: Stack(
-                              children: [
-                                ClipRRect(
-                                  child: Image.network(
-                                    widget.recipes[index].imageUrl,
-                                    fit: BoxFit.cover,
-                                    width: widthPercent(context, 100),
-                                    height: heightPercent(context, 25),
-                                  ),
-                                  borderRadius:
-                                  BorderRadius.circular(20),
+                            Positioned( //label
+                              width: widthPercent(context, 80),
+                              left: widthPercent(context, 3),
+                              bottom: heightPercent(context, 1.2),
+
+                              child: Text(widget.recipes[index].label, style: TextStyle(
+                                backgroundColor: Theme.of(context).backgroundColor == Color(0xff616161)? Theme.of(context).backgroundColor.withOpacity(.5) : Colors.white.withOpacity(.5), //checking if color is black or not
+                                color: Theme.of(context).textTheme.bodyText1!.color,
+                                fontSize: averagePercent(context, 3),
+                                fontStyle: FontStyle.italic,
+                              )
+
+                              ),),
+                            Positioned( //calories
+                              right: widthPercent(context, 3),
+                              top: heightPercent(context, 1.2),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).backgroundColor == Color(0xff616161)? Theme.of(context).backgroundColor.withOpacity(.5) : Colors.white.withOpacity(.5),
                                 ),
-                                Positioned( //label
-                                  width: widthPercent(context, 80),
-                                  left: widthPercent(context, 3),
-                                  bottom: heightPercent(context, 1.2),
+                                child: Row(
 
-                                  child: Text(widget.recipes[index].label, style: TextStyle(
-                                    backgroundColor: Theme.of(context).backgroundColor == Color(0xff616161)? Theme.of(context).backgroundColor.withOpacity(.5) : Colors.white.withOpacity(.5), //checking if color is black or not
-                                    color: Theme.of(context).textTheme.bodyText1!.color,
-                                    fontSize: averagePercent(context, 3),
-                                    fontStyle: FontStyle.italic,
-                                  )
-
-                                  ),),
-                                Positioned( //calories
-                                  right: widthPercent(context, 3),
-                                  top: heightPercent(context, 1.2),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Theme.of(context).backgroundColor == Color(0xff616161)? Theme.of(context).backgroundColor.withOpacity(.5) : Colors.white.withOpacity(.5),
+                                  children: [
+                                    Icon(Icons.local_fire_department_outlined,
+                                      color: Theme.of(context).textTheme.bodyText1!.color,
                                     ),
-                                    child: Row(
+                                    Text(widget.recipes[index].calories.substring(0,min(6, widget.recipes[index].calories.length)), style: TextStyle(
+                                      // backgroundColor: Theme.of(context).backgroundColor.withOpacity(.4),
+                                      color: Theme.of(context).textTheme.bodyText1!.color,
+                                      fontSize: averagePercent(context, 3),
+                                      fontStyle: FontStyle.italic,
+                                    )
 
-                                      children: [
-                                        Icon(Icons.local_fire_department_outlined,
-                                          color: Theme.of(context).textTheme.bodyText1!.color,
-                                        ),
-                                        Text(widget.recipes[index].calories.substring(0,min(6, widget.recipes[index].calories.length)), style: TextStyle(
-                                          // backgroundColor: Theme.of(context).backgroundColor.withOpacity(.4),
-                                          color: Theme.of(context).textTheme.bodyText1!.color,
-                                          fontSize: averagePercent(context, 3),
-                                          fontStyle: FontStyle.italic,
-                                        )
-
-                                        ),
-                                      ],
                                     ),
-                                  ),),
-                              ],
+                                  ],
+                                ),
+                              ),),
+                          ],
 
-                            )));
-                  }),
-            ],
-          )
+                        )));
+              }),
         ),
       ),
     );
   }
 }
-
-
